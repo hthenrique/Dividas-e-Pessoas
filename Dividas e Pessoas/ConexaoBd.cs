4ade69dd63ebbec1509edf8b3ceab9c2b9ff51ae
hthenrique <ht.henrique@live.com>
2020-11-08 00:07:33 -0300

    CRUD banco de dados

--- Dividas e Pessoas/ConexaoBd.cs
+++ Dividas e Pessoas/ConexaoBd.cs
@@ -15,5 +15,29 @@ namespace Dividas_e_Pessoas
         SqlDataReader dataReader;
         string strSql;
 
+        public class conexaoBanco
+        {
+            private string nomePessoa;
+            private string telefonePessoa;
+            private string celPessoa;
+            private string endereco;
+            private string divida;
+            private string parcelas;
+            private string parcelasPagas;
+            private string observ;
+
+            public conexaoBanco(string nomePessoa, string telefonePessoa, string celPessoa, string endereco, string divida, string parcelas, string parcelasPagas, string observ)
+            {
+                this.nomePessoa = nomePessoa;
+                this.telefonePessoa = telefonePessoa;
+                this.celPessoa = celPessoa;
+                this.endereco = endereco;
+                this.divida = divida;
+                this.parcelas = parcelas;
+                this.parcelasPagas = parcelasPagas;
+                this.observ = observ;
+            }
+        }
+
     }
 }
