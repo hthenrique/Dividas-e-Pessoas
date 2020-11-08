ef739de3c03a5c679b657c35be29b765833e61ab
hthenrique <ht.henrique@live.com>
2020-11-07 23:50:21 -0300

    banco de dados

--- Dividas e Pessoas/ConexaoBd.cs
+++ Dividas e Pessoas/ConexaoBd.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Dividas_e_Pessoas
 {
     class ConexaoBd
     {
+        SqlConnection conexao;
+        SqlCommand comando;
+        SqlDataAdapter dataAdapter;
+        SqlDataReader dataReader;
+        string strSql;
+
     }
 }
